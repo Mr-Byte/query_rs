0d9a7c6ba5773b22af4ff3f3f6388a1de9353234
Mr. Byte <bytemr@gmail.com>
2015-04-10 20:24:27 -0600

    Changed syntax of from clauses to allow for patterns, added flat_map from clauses, and added several more tests.

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,17 @@ macro_rules! query {
 }
 
 macro_rules! query_from {
-   (from $context:ident in $source:expr, $($remainder:tt)+) => {
+    (from $context:pat => $source:expr, $($remainder:tt)+) => {
         query_from!($source, $context => $($remainder)+)
     }; 
+    
+    ($source:expr, $context:pat => from $newContext:pat => $newSource:expr, $($remainder:tt)+) => {{
+        let source = $source.flat_map(|value| {
+            let $context = value;
+            $newSource 
+        });
+        query_from!(source, $newContext => $($remainder)+)
+    }};
     
     ($($tokens:tt)*) => {
         query_operator!($($tokens)*)
@@ -46,6 +54,12 @@ macro_rules! query_operator {
         }
     };
     
+    ($source:expr, $context:pat => from $($remainder:tt)+) => {
+        {
+            query_from!($source, $context => from $($remainder)+)
+        }
+    };
+    
     ($($tokens:tt)*) => {
         query_end!($($tokens)*)
     };
@@ -58,10 +72,10 @@ macro_rules! query_end {
 }
 
 #[test]
-fn where_should_remove_filtered_items() {
+fn where_clause_should_remove_filtered_items() {
     let source = vec! [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    let result = query! { from x in source.into_iter(),
+    let result = query! { from x => source.into_iter(),
                           where x % 2 == 0,
                           select x };
 
@@ -73,8 +87,26 @@ fn where_should_remove_filtered_items() {
 }
 
 #[test]
-fn let_should_introduce_new_context() {
-    let result = query! { from x in (0..10).zip(0..10),
+fn where_clause_should_be_able_to_take_closure() {
+    let source = vec! [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let threshold = 3;
+    
+    let result = query! { from x => source.into_iter(),
+                          where x > threshold,
+                          select x };
+
+    let expected = vec! [4, 5, 6, 7, 8, 9, 10];
+ 
+    for (i, x) in result.enumerate() {
+        assert_eq!(x, expected[i]);
+    }
+}
+
+#[allow(unused_variables)]
+#[test]
+fn let_clause_should_introduce_new_context() {
+    let result = query! { from x => (0..10).zip(0..10),
                           let (a, b) = x,
                           select a*b };
 
@@ -85,3 +117,61 @@ fn let_should_introduce_new_context() {
     }
 }
 
+#[allow(unused_variables)]
+#[test]
+fn let_clause_should_be_able_to_take_closure() {
+    let multiplier = 2;
+
+    let result = query! { from x => (0..5),
+                          let a = x * multiplier,
+                          select a };
+
+    let expected = vec! [0, 2, 4, 6, 8];
+ 
+    for (i, x) in result.enumerate() {
+        assert_eq!(x, expected[i]);
+    }
+}
+
+#[test]
+fn from_clause_should_allow_patterns() {
+    let result = query! { from (a, b) => (0..5).zip(0..5),
+                          select a * b };
+                          
+    let expected = [0, 1, 4, 9, 16];
+    
+    for (i, x) in result.enumerate() {
+        assert_eq!(x, expected[i]);
+    } 
+}
+
+#[test]
+fn multiple_from_clauses_should_flat_map() {
+    let input = [[0, 1], [2, 3], [4, 5]];
+
+    let result = query! { from xs => input.iter(),
+                          from &y => xs.iter(),
+                          select y };
+
+    let expected = vec! [0, 1, 2, 3, 4, 5];
+ 
+    for (i, x) in result.enumerate() {
+        assert_eq!(x, expected[i]);
+    }
+}
+
+#[test]
+fn an_operator_between_from_clauses_should_flat_map_the_result_of_the_operator() {
+    let input = vec![vec! [0], vec! [1, 2], vec! [3, 4, 5]];
+
+    let result = query! { from xs => input.iter(),
+                          where xs.len() > 1,
+                          from &y => xs.iter(),
+                          select y };
+
+    let expected = vec! [1, 2, 3, 4, 5];
+ 
+    for (i, x) in result.enumerate() {
+        assert_eq!(x, expected[i]);
+    }
+}
